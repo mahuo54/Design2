dfd5436d840ededae884f8d8c461e52c8c1e2b39
jeanz <jeanz@192.168.2.19>
2020-02-09 20:26:37 -0500

    Plein de tests à fréquence d'échantillonage de 2000Hz

--- Library/DataAnalysis/TF.m
+++ Library/DataAnalysis/TF.m
@@ -1,8 +1,8 @@
-for k =2:22
-    L=length(position.Data(20:end,k));
-    Fs=150;                           % or whatever is actual sampling frequency
-    y = position.Data(20:end,k);
-    f = Fs*linspace(0,1,L/2+1);         % single-sided positive frequency
+for k =2:10
+    L=length(out.position_5.Data(:,k));
+    Fs=2000;                           % or whatever is actual sampling frequency
+    y = out.position_5.Data(20:end,k);
+    f = Fs*(0:(L/2))/L;         % single-sided positive frequency
     X = fft(y)/L;                     % normalized fft
     PSD=2*abs(X(1:L/2+1));            % one-sided amplitude spectrum
     plot(f, PSD);
--- animation.m
+++ animation.m
@@ -5,11 +5,11 @@ xi = linspace(min(x), max(x), 150);
 %h = figure;
 
 %On choisit les axes en fonction de ymax
-ymax = 1.25*max(max(max(position)),max(abs(min(position))))
+ymax = 1.25*max(max(max(position_21)),max(abs(min(position_21))))
 
 %filename = 'testAnimated.gif'
-for k = 1 : length(position.Time)
-    y = position.Data(k,x);
+for k = 1 : length(position_21.Time)
+    y = position_21.Data(k,x);
     yi = interp1(x, y, xi, 'spline', 'extrap');
   plot(xi, yi, '-r');
   title('Animation de la corde');
@@ -19,7 +19,7 @@ for k = 1 : length(position.Time)
   ylim([-1*ymax ymax])
   xlabel('Segments de corde')
   ylabel('Position')
-  pause( 0.05 );
+  pause( 0.0005 );
   
   %Faire un GIF
   %frame = getframe(h); 
