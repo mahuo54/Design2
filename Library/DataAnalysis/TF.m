a7a19aba8aa75d47366bfe69e0f870b48b44d613
jeanz <jeanz@192.168.2.19>
2020-02-10 23:06:37 -0500

    Finition de la simulation de la corde (corde2) par calcul matriciel, résultats beaucoup plus rapide, peut être utiliser pour déterminer le bon nmobre de segments à utiliser.

--- Library/DataAnalysis/TF.m
+++ Library/DataAnalysis/TF.m
@@ -1,7 +1,7 @@
-for k =2:6
-    L=length(out.position_5.Data(:,k));
+for k =1:21
+    L=length(out.corde_mesure.Data(:,k));
     Fs=2000;                           % or whatever is actual sampling frequency
-    y = out.position_5.Data(20:end,k);
+    y = out.corde_mesure.Data(20:end,k);
     f = Fs*(0:(L/2))/L;         % single-sided positive frequency
     X = fft(y)/L;                     % normalized fft
     PSD=2*abs(X(1:L/2+1));            % one-sided amplitude spectrum
