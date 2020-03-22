19fefc392b0cbae2c156f2ddb991ea4232e6df91
Max <maxime.huot.2@gmail.com>
2020-03-22 14:48:49 -0400

    App is running fine. Selected values produce a singularities, have to check again what are the default values

--- RunSingleSimulation.m
+++ RunSingleSimulation.m
@@ -6,4 +6,11 @@ p.pos_capteur_relative = 0.4;
 test =  SystemSimulator();
 results = test.RunSimulation(p);
 
-    
+
+SimulationParams = SimulationParameterManager();
+SimulationParams.SetToDefault();
+
+simulator = SystemSimulator();
+simulatorManager = SimulatorManager(simulator);
+
+simulatorManager.Simulate(SimulationParams.EnumerateSimulationParameters());
